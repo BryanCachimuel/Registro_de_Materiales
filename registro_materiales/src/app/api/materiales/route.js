7647b5f01d61fc46ea81a9eaefec04682be9bb47
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-04-19 20:07:53 -0500

    Realizando el endpoint para crear nuevos materiales

--- registro_materiales/src/app/api/materiales/route.js
+++ registro_materiales/src/app/api/materiales/route.js
@@ -1,10 +1,18 @@
-import connectDB from '@lib/db';
+import connectDB from '@/lib/db';
 import { NextResponse } from 'next/server';
+import { MaterialModel } from '@/models/Material';
 
-export const GET = async () => {
-
+export const POST = async (req, res) => {
+    await connectDB()
+    try {
+        const body = await req.json()
+        const nuevoMaterial = await MaterialModel.create(body)
+        return NextResponse.json({data:nuevoMaterial}, {status:200})
+    } catch (error) {
+        return NextResponse.json({data: null}, {status:500})
+    }
 }
 
-export const POST = async () => {
+export const GET = async () => {
 
 }
