b70a65c31955f3b5ff72f13bf436ee4935ef8419
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-04-19 21:15:05 -0500

    Elaborando el endpoint para actualizar un documento mediante unparámetro de identificador

--- registro_materiales/src/app/api/materiales/[id]/route.js
+++ registro_materiales/src/app/api/materiales/[id]/route.js
@@ -15,6 +15,7 @@ export const GET = async (request, {params}) => {
     }
 }
 
+// Eliminar un documento
 export const DELETE = async (request, {params}) => {
     await connectDB()
     const id = params.id
@@ -24,7 +25,30 @@ export const DELETE = async (request, {params}) => {
             return NextResponse.json({message: `Documento con ID: ${id} no encontrado`}, {status:404})
         }
         return NextResponse.json({data:materialEliminar}, {status:200})
-        
+
+    } catch (error) {
+        return NextResponse.json({data: null}, {status:500})
+    }
+}
+
+/* Actualizar un documento 
+$set:{..body} -> objeto que contiene los campos y los nuevos valores
+$set: -> operador de actualización de MongoDB que establece los valores
+         especificados en el objeto
+{new:true} -> le indica a findByAndUpdate() que devuelve el documento 
+              actualizado en lugar del documento original
+*/
+export const PUT = async (request, {params}) => {
+    await connectDB()
+    const id = params.id
+    const body = await request.json()
+    try {
+        const materialActualizar = await MaterialModel.findByIdAndUpdate(id, {$set:{...body}}, {new:true})
+        if(!materialActualizar){
+            return NextResponse.json({message: `Documento con ID: ${id} no encontrado`}, {status:404})
+        }
+        return NextResponse.json({data:materialActualizar}, {status:200})
+
     } catch (error) {
         return NextResponse.json({data: null}, {status:500})
     }
