4785004bc4e1d5dc2bf9a1e29626192fdc2bc820
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-04-19 20:28:49 -0500

    Creando el endpint para listar todos los registros realizados dentro de la base de datos

--- registro_materiales/src/app/api/materiales/route.js
+++ registro_materiales/src/app/api/materiales/route.js
@@ -7,12 +7,18 @@ export const POST = async (req, res) => {
     try {
         const body = await req.json()
         const nuevoMaterial = await MaterialModel.create(body)
-        return NextResponse.json({data:nuevoMaterial}, {status:200})
+        return NextResponse.json({data:nuevoMaterial}, {status:201})
     } catch (error) {
         return NextResponse.json({data: null}, {status:500})
     }
 }
 
 export const GET = async () => {
-
+    await connectDB()
+    try {
+        const listadoMateriales = await MaterialModel.find({})
+        return NextResponse.json({data:listadoMateriales}, {status:200})
+    } catch (error) {
+        return NextResponse.json({data: null}, {status:500})
+    }
 }
