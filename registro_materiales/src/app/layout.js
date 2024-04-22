923bc85193bd4d52a27c4172160c3e8c37ee3104
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-04-21 21:57:54 -0500

    Integrando el componente Navbar en el layout del proyecto

--- registro_materiales/src/app/layout.js
+++ registro_materiales/src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import NavBar from "@/components/NavBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +12,18 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="h-screen">
+          {/* componente Navbar */}
+          <NavBar/>
+
+          {/* page.js página principal */}
+          <div className="mt-8 mx-auto p-4">
+            {children}
+          </div>
+
+        </div>
+      </body>
     </html>
   );
 }
