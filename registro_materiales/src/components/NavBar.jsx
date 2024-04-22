bdf660830d61345509a3cdd93af5d3b55fa1c159
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-04-21 21:54:18 -0500

    Agregando un logo como link de redirección para llegar a la pantalla de inicio y agregando un boton de crear para redirigir hacia la vista de creación de nuevos materiales

--- registro_materiales/src/components/NavBar.jsx
+++ registro_materiales/src/components/NavBar.jsx
@@ -1,8 +1,26 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+   <nav className="flex justify-between items center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white px-8 py-3">
+      <Link href={"/"}>
+        <Image
+          src="/logo.png"
+          alt="Logo Construccion"
+          width={50}
+          height={25}
+          priority
+        />
+      </Link>
+      <Link 
+        className="inline-flex items-center px-4 py-2 font-medium text-center text-white bg-slate-700 hover:bg-slate-900 rounded-lg"
+        href={"/create"}
+      >
+        Crear
+      </Link>
+   </nav>
   )
 }
 
